919d63bf4089348f00cfe0803f2bdd77a0d3e39d
udjinia <udjinia@gmail.com>
2026-01-18 09:26:38 -0500

    step in initial prediction

--- app.py
+++ app.py
@@ -3,10 +3,16 @@ from models import GlobalInfo, Model
 
 app = Flask(__name__, static_folder='static')
 
+GlobalInfoObj = GlobalInfo()
+
 # Home page - serves index.html
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/api/models', methods=['GET'])
+def get_available_models():
+    return jsonify(GlobalInfoObj.get_all_models_info())
 
 @app.route('/api/predict_ml', methods=['POST'])
 def predict_ml():
@@ -15,6 +21,8 @@ def predict_ml():
 
     # Should validate data here first (or maybe in model)
     # GlobalInfoObj.get_model('random_forest_model').predict(data)
+ 
+    # Надо добавить проверку - какой моделью должна проверяться фигня
     prediction, probability = GlobalInfoObj.get_model('xgboost_model').predict(data)
 
     print("Prediction result:", prediction, probability)
@@ -25,8 +33,6 @@ def predict_ml():
         'status': 'success'
     })
 
-GlobalInfoObj = GlobalInfo()
-
 if __name__ == '__main__':
     # Create models directory if it doesn't exist
     if not os.path.exists('models'):
@@ -34,7 +40,7 @@ if __name__ == '__main__':
 
     GlobalInfoObj.add_model('xgboost_model', 'scaler')
     GlobalInfoObj.add_model('random_forest_model', 'scaler')
-    # GlobalInfoObj.add_model('initial_data_random_forest_model', 'initial_data_scaler')
+    GlobalInfoObj.add_model('initial_data_random_forest_model', 'initial_data_scaler', is_initial = True)
 
     # Run the server
     app.run(host='0.0.0.0', port=5000, debug=True)
--- models.py
+++ models.py
@@ -1,9 +1,18 @@
 
 from prelude import *
 
+# =====  Move it later in utils ===== 
+
+def filter_array(source_array, reference_array):
+    """Фильтрует source_array, оставляя только элементы из reference_array"""
+    reference_set = set(reference_array)
+    return [item for item in source_array if item in reference_set]
+# ========== 
+
 # data prepare part
 
-INITIAL_FEASTURES_NAME = [
+# order of names is important
+INITIAL_FEASTURES_NAMES = [
         'age_onset', 'heredity', 'smoking_status', 'sex',
         'us1_thyroid_volume', 'us1_nodules', 'us1_nodules_cm',
         'tsh_1', 'ft4_1', 'ft3_1', 'ft3_to_ft4_ratio',
@@ -14,7 +23,8 @@ READMISSION_FEATURE_NAMES = [
         'treatment_type', 'tsh_3', 'us3_thyroid_volume', 'us3_nodules', 'us3_nodules_cm'
 ]
 
-ALL_FEATURE_NAMES = INITIAL_FEASTURES_NAME + READMISSION_FEATURE_NAMES
+# thanks to good luck - ALL_FEATURE_NAMES order is same as inital case + readmission case
+ALL_FEATURE_NAMES = INITIAL_FEASTURES_NAMES + READMISSION_FEATURE_NAMES
 
 # Define categorical and numerical columns
 CATEGORICAL_COLS = [
@@ -56,46 +66,53 @@ def load_ml_model(model_path, scaler_path):
         print(f"Error loading model: {str(e)}")
         return None, None
 
-def extract_features(data):
+def extract_features(data, features_names_list):
     return [
         data.get(name, -1) 
-        for name in ALL_FEATURE_NAMES
+        for name in features_names_list
     ]
 
-def prepare_features(features, scaler):
-
+def prepare_features(features, scaler, features_names_list):
     # Create DataFrame with proper column names
-    features_df = pd.DataFrame([features], columns=ALL_FEATURE_NAMES)
+    features_df = pd.DataFrame([features], columns=features_names_list)
 
     print(f"DataFrame shape: {features_df.shape}")
 
+    numerical_cols_names = filter_array(NUM_COLS, features_names_list)
     # Convert to numpy array for prediction
     if scaler is not None:
         # Scale only numerical columns
-        features_df[NUM_COLS] = scaler.transform(features_df[NUM_COLS])
-        print(f"Scaled numerical columns: {NUM_COLS}")
+        features_df[numerical_cols_names] = scaler.transform(features_df[numerical_cols_names])
+        print(f"Scaled numerical columns: {numerical_cols_names}")
 
     # Convert to numpy array for prediction
     return features_df.values
 
 
 class Model:
-    def __init__(self, model_name, scaler_name):
+    def __init__(self, model_name, scaler_name, is_initial):
         self.model_name = model_name
         self.scaler_name = scaler_name
+        # If models only for inital_cases - it works withanother set of params
+        self.all_features_names = INITIAL_FEASTURES_NAMES if is_initial else ALL_FEATURE_NAMES
+        self.is_initial = is_initial
         self._load()
 
-    def _genPaths(self, base='models'):
+    def _gen_paths(self, base='models'):
         # Think about extensions
         return (os.path.join(base, self.model_name + '.pkl'), os.path.join(base, self.scaler_name + '.pkl'))
 
     def _load(self):
-        (model_path, scaler_path) = self._genPaths() 
+        (model_path, scaler_path) = self._gen_paths() 
         self.model, self.scaler = load_ml_model(model_path, scaler_path)
+
+    def get_type(self):
+        return 'init' if self.is_initial else 'follow-up'
 
     def predict(self, data):
         # Maybe extract_features should be done outside of this method
-        features_array = prepare_features(extract_features(data), self.scaler)
+        # TODO: sign about architecture problems - one parametr in two places
+        features_array = prepare_features(extract_features(data, self.all_features_names), self.scaler, self.all_features_names)
 
         # Make prediction
         prediction = self.model.predict(features_array)
@@ -105,8 +122,12 @@ class Model:
 class GlobalInfo:
     models = dict()
 
-    def add_model(self, model_name, scaler_name):
-        self.models[model_name] = Model(model_name, scaler_name)
+    def add_model(self, model_name, scaler_name, is_initial = False):
+        # TODO: add exception on model's addition fail
+        self.models[model_name] = Model(model_name, scaler_name, is_initial)
     
     def get_model(self, model_name):
         return self.models.get(model_name)
+
+    def get_all_models_info(self):
+        return [{'name':model_name, 'type': model.get_type()} for [model_name, model] in self.models.items()]
